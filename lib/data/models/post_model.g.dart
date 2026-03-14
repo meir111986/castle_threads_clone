927e015f2d8f5af28143257e1874c9cd4abb5ee1
meir111986 <meir111986@gmail.com>
2026-03-14 13:51:34 +0500

    Первый день проекта threads_clone

---
+++ lib/data/models/post_model.g.dart
@@ -0,0 +1,53 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'post_model.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class PostModelAdapter extends TypeAdapter<PostModel> {
+  @override
+  final typeId = 0;
+
+  @override
+  PostModel read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return PostModel(
+      id: fields[0] as String,
+      content: fields[1] as String,
+      authorId: fields[2] as String,
+      createdAt: fields[3] as String,
+      likes: (fields[4] as num).toInt(),
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, PostModel obj) {
+    writer
+      ..writeByte(5)
+      ..writeByte(0)
+      ..write(obj.id)
+      ..writeByte(1)
+      ..write(obj.content)
+      ..writeByte(2)
+      ..write(obj.authorId)
+      ..writeByte(3)
+      ..write(obj.createdAt)
+      ..writeByte(4)
+      ..write(obj.likes);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is PostModelAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
---
+++ lib/presentation/screens/feed_screen.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:threads_clone/domain/entities/post.dart';
+import 'package:threads_clone/presentation/widgets/post_card.dart';
+
+class FeedScreen extends StatelessWidget {
+  const FeedScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final posts = [
+      Post(
+        id: '1',
+        content: 'Красивый день в Астане',
+        authorId: '1',
+        createdAt: DateTime.now().toString(),
+        likes: 3,
+      ),
+      Post(
+        id: '2',
+        content: 'Working on my Flutter project!',
+        authorId: '2',
+        createdAt: DateTime.now().toString(),
+        likes: 6,
+      ),
+      Post(
+        id: '3',
+        content: 'Знакомтесь это мой новый пост!',
+        authorId: '3',
+        createdAt: DateTime.now().toString(),
+        likes: 9,
+      ),
+    ];
+
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          'Threads v2.0',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: ListView.separated(
+        padding: EdgeInsets.symmetric(vertical: 8),
+        itemBuilder: (context, index) {
+          final post = posts[index];
+          return PostCard(post: post);
+        },
+        separatorBuilder: (_, _) => Divider(height: 1),
+        itemCount: posts.length,
+      ),
+    );
+  }
+}
