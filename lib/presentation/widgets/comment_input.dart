0a5063a3cbce1965ac78fa6f71db96ea54e452a8
meir111986 <meir111986@gmail.com>
2026-03-31 16:56:28 +0500

    Реализовал CommentInput добавление коментарии

--- lib/presentation/widgets/comment_input.dart
+++ lib/presentation/widgets/comment_input.dart
@@ -1,4 +1,7 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:threads_clone/presentation/bloc/comments/comments_cubit.dart';
+import 'package:threads_clone/presentation/bloc/comments/comments_state.dart';
 
 class CommentInput extends StatefulWidget {
   const CommentInput({super.key});
@@ -8,8 +11,58 @@ class CommentInput extends StatefulWidget {
 }
 
 class _CommentInputState extends State<CommentInput> {
+  late TextEditingController _controller;
+
+  @override
+  void initState() {
+    _controller = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return SizedBox.shrink();
+    return Padding(
+      padding: EdgeInsets.all(16),
+      child: Row(
+        children: [
+          const CircleAvatar(radius: 20, child: Icon(Icons.person)),
+          Expanded(
+            child: TextFormField(
+              controller: _controller,
+              onChanged: context.read<CommentsCubit>().inputChanged,
+              decoration: InputDecoration(
+                hintText: 'Комментарий...',
+                border: InputBorder.none,
+                hintStyle: TextStyle(fontSize: 14),
+                contentPadding: EdgeInsets.symmetric(
+                  horizontal: 12,
+                  vertical: 8,
+                ),
+              ),
+            ),
+          ),
+
+          BlocBuilder<CommentsCubit, CommentsState>(
+            builder: (context, state) {
+              return IconButton(
+                icon: const Icon(Icons.send),
+                onPressed: state.canSubmit
+                    ? () {
+                        context.read<CommentsCubit>().addComment();
+                        _controller.clear();
+                      }
+                    : null,
+              );
+            },
+          ),
+        ],
+      ),
+    );
   }
 }
